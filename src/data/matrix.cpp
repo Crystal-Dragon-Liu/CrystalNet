84c9a4fa89ae3814911728dae98d5371f604acaf
LybCrystal <LybCrystal@163.com>
2021-11-02 13:13:43 +0800

    override assignment operator

--- src/data/matrix.cpp
+++ src/data/matrix.cpp
@@ -1,8 +1,14 @@
 #include "include/data/matrix.h"
+#include "memory.h"
 
+/*
+    @brief  the method under MatrixFunc namespace provide the general operation,
+            to process matrix data.
+*/
 namespace MatrixFunc{
 
     float** make2DArray(size_t rows, size_t cols){
+        if(rows == 0 || cols == 0) return nullptr;
         float** rawData = dataC2DAllocator::allocate(rows, sizeof(float*));
         for(size_t i = 0; i < rows; i++){
             rawData[i] = dataCitemAllocator::allocate(cols, sizeof(float));}
@@ -11,17 +17,17 @@ namespace MatrixFunc{
 
     Matrix* copyMatrix(Matrix* matrix){return nullptr;}
 
+    void freeRawData(float** rawData, size_t row_count){
+    if(rawData){
+        for(size_t i = 0; i < row_count; i++){dataItemAllocator::deallocate(rawData[i]);}
+        data2DAllocator::deallocate(rawData);}
+    }
+
 }
 
 
 Matrix::Matrix(): row_count_(0), col_count_(0), rawdata_(nullptr){}
-
-Matrix::~Matrix(){
-    if(rawdata_){
-        for(size_t i = 0; i < row_count_; i++){dataItemAllocator::deallocate(rawdata_[i]);}
-        data2DAllocator::deallocate(rawdata_);
-    }
-}
+Matrix::~Matrix(){MatrixFunc::freeRawData(rawdata_, row_count_);}
 
 Matrix::Matrix(size_t rows, size_t cols){
     row_count_      =   rows;
@@ -29,6 +35,20 @@ Matrix::Matrix(size_t rows, size_t cols){
     float** rawData =   MatrixFunc::make2DArray(row_count_, col_count_);
     this->rawdata_  =   rawData;   
 }
+
+Matrix& Matrix::operator=(Matrix& data){
+    // delete raw data.
+    if(this == &data) return *this;
+    float** rawdataTemp = rawdata_;
+    rawdata_ = MatrixFunc::make2DArray(data.row_count_, data.col_count_);
+    // copy data.
+    float** otherData = data.getData();
+    for(size_t i = 0; i < data.row_count_;){
+        memcpy(rawdata_[i], otherData[i], data.getColCount()*sizeof(float));}
+    MatrixFunc::freeRawData(rawdataTemp, row_count_);
+    return *this;
+}
+
 
 
 
--- src/include/data/matrix.h
+++ src/include/data/matrix.h
@@ -25,12 +25,20 @@ public:
         float** getData(){ return rawdata_; }
         size_t  getRowCount() const {return row_count_;}
         size_t  getColCount() const {return col_count_;}
+        Matrix& operator=(Matrix& data);
 private:
+        /* maybe copy constructor should be forbidden. */
+        Matrix(const Matrix& matrix){}
         size_t  row_count_;
         size_t  col_count_;
         float** rawdata_;
 };
 
+
+/*
+    @brief  the method under MatrixFunc namespace provide the general operation,
+            to process matrix data.
+*/
 namespace MatrixFunc{
         /*
                 @brief  create a new Matrix with size (rows, cols).
@@ -38,10 +46,17 @@ namespace MatrixFunc{
                 @param  rows: row counts.
                 @param  cols: column counts.
         */
-        extern float** make2DArray(size_t rows, size_t cols);
+        extern float**  make2DArray(size_t rows, size_t cols);
         
         //TODO create a brand new Matrix obj with same data from matrix expected.
-        extern Matrix*    copyMatrix(Matrix* matrix);      
+        extern Matrix*  copyMatrix(Matrix* matrix);
+
+        /*      @brief  free the raw data which is usually belong to Matrix obj.
+                @param  rawData: the data with type float**.
+                @param  row_count: the row count of data.       
+        */
+        extern void     freeRawData(float** rawData, size_t row_count);
+      
 }
 
 #endif 
