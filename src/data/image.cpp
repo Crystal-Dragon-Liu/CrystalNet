5a3784b455dd32e28e2db3b6f95c5708d6168b09
LybCrystal <LybCrystal@163.com>
2021-11-07 21:40:50 +0800

    add some declare which is useless.

--- src/data/image.cpp
+++ src/data/image.cpp
@@ -4,4 +4,15 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+namespace ImageFunc{
+
+    Image* loadImage(const char* fileName, size_t w, size_t h, size_t channel){return nullptr;}
+    
+    Image* _loadImage(const char* fileName, size_t channels){return nullptr;}
+
+    void   freeImage(Image* data){}
+
+    Image* resizeImage(Image* og_data, size_t w, size_t h){return nullptr;}
+
+}
 
--- src/include/data/image.h
+++ src/include/data/image.h
@@ -15,5 +15,31 @@ private:
     float* data_;
 };
 
+namespace ImageFunc{
+    /*
+        @brief  load a image from filename expected.
+        @param filename:    file path.
+        @param w:           width of image.
+        @param h:           height of image.
+        @param channel:     channel of 
+    */
+    extern Image* loadImage(const char* fileName, size_t w, size_t h, size_t channel);
+
+    /*
+        @brief  load a image by calling the func from stb_image lib.
+    */
+    extern Image* _loadImage(const char* fileName, size_t channels);
+
+    /*
+        @brief  free image data.
+    */
+    extern void   freeImage(Image* data);
+
+    /*
+        @brief resize image input, and create a new image with size (w, h)
+    */
+    extern Image* resizeImage(Image* og_data, size_t w, size_t h);
+
+}
 
 #endif
